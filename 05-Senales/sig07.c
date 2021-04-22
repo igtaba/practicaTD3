d67547811a9e567bd4835e68b8431803627b1048
ignacio.tabanera@outlook.com <ignacio.tabanera@outlook.com>
2021-04-22 11:10:16 -0300

    4th Commit, 05-Senales, ex: 07

--- 05-Senales/sig07.c
+++ 05-Senales/sig07.c
@@ -1,5 +1,5 @@
 /*
- Ejercicio 6
+ Ejercicio 7
 */
 
 #include <stdio.h>
@@ -9,26 +9,74 @@
 #include <sys/wait.h>
 #include <signal.h>
 
-
-void manejador(int a)
+void handler_sigchild(int x)
 {
-	write(STDOUT_FILENO,"\nMe rehuso a terminar\n",sizeof("\nMe rehuso a terminar\n"));  
+	write(STDOUT_FILENO, "\nChild Ended\n",sizeof("\nChild Ended\n"));
 }
 
 int main ()
 {
+	pid_t aux1, aux2, aux3;
+	pid_t PIDson1, PIDson2, PIDson3; //I CANNOT USE THE PID's TO END THE PROCESS IN THE FATHER COZ THEY'RE NOT IN THE FATHER (have rubbish value), THEY ASSUME THE PID VALUE ONLY IN THE SON MEMORY 
+	
+	signal(SIGCHLD, handler_sigchild);
+   
+  	printf("Proceso PID = %d\n", getpid());   
+   
+   	aux1 = fork(); // Creation of 1st Son
+   	
+   	if(aux1 == 0) //Code that Son 1 is going to execute
+   	{
+   		PIDson1 = getpid();
+   		printf("sSon 1 has PID = %d\n", PIDson1);
+   		while (1);
+   	}
+   	else 
+   	{
+	   	aux2 = fork();  // Creation of 2nd Son
+	   	
+	   	if(aux2 == 0) //Code that Son 2 is going to execute
+	   	{
+	   		PIDson2 = getpid();
+	   		printf("Son 2 has PID = %d\n", PIDson2);
+	   		while (1);
+	   	}
+   		else
+   		{
+		   	aux3 = fork();  // Creation of 3rd Son
+		   	
+		   	if(aux3 == 0) //Code that Son 3 is going to execute
+		   	{
+		   		PIDson3 = getpid();
+		   		printf("Son 3 has PID = %d\n", PIDson3);
+		   		while (1);
+		   	}
+			else //Code that the Father is going to execute
+			{
+		   		sleep(2);
 
-   //signal(SIGKILL, SIG_IGN);
-   signal(SIGUSR1, manejador);  //Ejecutamos el manejador para SIGUSR1
+				kill(aux1, SIGKILL);
+				   	
+			   	sleep(2);
+				   	
+				kill(aux2, SIGKILL);
+				
+				sleep(2);
+				   	
+				kill(aux3, SIGKILL);
+				   	
+		   	}
+
+		}
+
+	}
+  	while(1);
    
-   printf("Proceso PID = %d\n", getpid());   
-   
-   while(1);
-   
-   exit(0);
+  	exit(0);
 }
 
+
 /*
-	 En este caso la señal SIGUSR1 si es manejable y es ignorada por el proceso cuando es enviada desde otra terminal. En ese caso se imprime por pantalla el mensaje deseado pero el proceso se mantiene corriendo.
+	
 	 
 */
